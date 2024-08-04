6dd06a2ecd1b22cf53942cfa7c46079a7077aeb5
ptduy14 <phantanduy14@gmail.com>
2024-08-04 12:15:57 +0700

    feat: complete setup laravel-permission package (SPATIE)

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The name of the table associated with the model.
