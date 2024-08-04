33243a8bc5d1ca31d268709ff8b63a18961629a3
ptduy14 <phantanduy14@gmail.com>
2024-08-04 21:56:17 +0700

    refactor: add more statics method & refactor custome exception (HTTPException)

--- app/Exceptions/HTTPException.php
+++ app/Exceptions/HTTPException.php
@@ -3,18 +3,20 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HTTPException extends Exception
 {
+    public static function NOT_FOUND($message = 'Resource not Found')
+    {
+        return new self($message, Response::HTTP_NOT_FOUND);
+    }
+
     public static function BAD_REQUEST($message = 'Bad Request')
     {
         return new self($message, Response::HTTP_BAD_REQUEST);
-    }
-
-    public static function NOT_FOUND($message = 'Not Found')
-    {
-        return new self($message, Response::HTTP_NOT_FOUND);
     }
 
     // Thêm các method khác nếu cần
@@ -31,5 +33,12 @@ class HTTPException extends Exception
     public static function INTERNAL_SERVER_ERROR($message = 'Internal Server Error')
     {
         return new self($message, Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    public function render($request): JsonResponse {
+        return response()->json([
+            'status' => $this->getCode(),
+            'message' => $this->getMessage()
+        ], $this->getCode());
     }
 }
